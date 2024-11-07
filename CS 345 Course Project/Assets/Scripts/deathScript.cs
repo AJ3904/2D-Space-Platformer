014ce2d69a4a0e8371ddc3120dcafb626739c037
AJ3904 <aj984@drexel.edu>
2024-11-06 19:15:20 -0500

    Fixed player respawn issue

--- CS 345 Course Project/Assets/Scripts/deathScript.cs
+++ CS 345 Course Project/Assets/Scripts/deathScript.cs
@@ -6,24 +6,11 @@ public class deathScript : MonoBehaviour
 {
     public GameObject startPoint;
     public GameObject Player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player")){
             Player.transform.position = startPoint.transform.position;
-
-            Player.SetActive(true);
         }
     }
 }
