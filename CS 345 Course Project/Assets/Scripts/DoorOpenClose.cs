33d101e09bb85aefd2a84732ec78c768394ae405
AJ3904 <aj984@drexel.edu>
2024-11-17 14:06:03 -0500

    Door animation added

---
+++ CS 345 Course Project/Assets/Scripts/DoorOpenClose.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenClose : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            animator.SetTrigger("Close");
+        }
+    }
+}
