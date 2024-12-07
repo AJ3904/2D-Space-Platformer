45e1e5d47d57a6f23e3f7319d4c2a106cd71e27c
sebas-tion <eso33@drexel.edu>
2024-12-06 22:09:50 -0500

    adjusted sound in level 0

--- CS 345 Course Project/Assets/Scripts/DoorOpenClose.cs
+++ CS 345 Course Project/Assets/Scripts/DoorOpenClose.cs
@@ -7,13 +7,23 @@ public class DoorOpenClose : MonoBehaviour
     private Animator animator;
     public PlayerController player;
 
-     public AudioClip doorOpenClip;
+    public AudioClip doorOpenClip;
     private AudioSource doorAudioSource;
+
+    [Range(0f, 1f)] // This makes the volume adjustable in the Unity Inspector with a slider
+    public float doorAudioVolume = 0.75f; // Default volume
+
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+
         doorAudioSource = gameObject.AddComponent<AudioSource>();
+        doorAudioSource.loop = false;
+
+        // Set the volume to the manually adjustable value
+        doorAudioSource.volume = doorAudioVolume;
     }
 
     void OnTriggerStay2D(Collider2D other)
