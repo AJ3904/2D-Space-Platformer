8e2fbff3580b4c8543db3c63f0b11b21b5d0acfc
Matthew Attieh <ma3854@drexel.edu>
2024-12-07 18:58:00 -0500

    Added Death Screen
    Death screen pop up added when player dies.

--- CS 345 Course Project/Assets/Scripts/deathScript.cs
+++ CS 345 Course Project/Assets/Scripts/deathScript.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class deathScript : MonoBehaviour
 {
     public GameObject startPoint;
     public GameObject Player;
+    public GameObject GameOverUI;
+
+    void Start()
+    {
+        GameOverUI.SetActive(false);
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player")){
-            Player.transform.position = startPoint.transform.position;
+            GameOverUI.SetActive(true);
+            Time.timeScale = 0f;
         }
+    }
+
+    public void RestartLevel()
+    {
+        GameOverUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+        GameOverUI.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
