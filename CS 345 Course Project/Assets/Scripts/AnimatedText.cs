ed5ad73371dcf512a0166c14defb64d04f1f9ba2
sebas-tion <eso33@drexel.edu>
2024-12-07 20:26:04 -0500

    Updated audio in opening scene

--- CS 345 Course Project/Assets/Scripts/AnimatedText.cs
+++ CS 345 Course Project/Assets/Scripts/AnimatedText.cs
@@ -13,30 +13,56 @@ public class AnimatedText : MonoBehaviour
     [SerializeField]
     private float dialogueSpeed = 0.01f;
 
+    public AudioClip textScrollClip;
+    private AudioSource textAudioSource;
+
+    void Start()
+    {
+        // Initialize the audio source
+        textAudioSource = gameObject.AddComponent<AudioSource>();
+        textAudioSource.clip = textScrollClip;
+        textAudioSource.loop = true; // Ensure the clip loops while text is being written
+        textAudioSource.playOnAwake = false; // Prevent audio from playing immediately
+    }
+
     void NextSentence()
     {
-        if(index < sentences.Length)
+        if (index < sentences.Length)
         {
             dialogueText.text = "";
-            if(index > 2)
+            if (index > 2)
             {
                 dialogueText.text = "Brace for impact in ";
                 dialogueSpeed = 1f;
             }
             StartCoroutine(WriteSentence());
-        } 
+        }
         else
         {
             animator.SetTrigger("Darken");
         }
     }
+
     IEnumerator WriteSentence()
     {
-        foreach(char character in sentences[index].ToCharArray())
+        // Start the audio when writing begins
+        if (textAudioSource != null && textScrollClip != null)
+        {
+            textAudioSource.Play();
+        }
+
+        foreach (char character in sentences[index].ToCharArray())
         {
             dialogueText.text += character;
             yield return new WaitForSeconds(dialogueSpeed);
         }
+
+        // Stop the audio when writing ends
+        if (textAudioSource != null)
+        {
+            textAudioSource.Stop();
+        }
+
         index++;
         NextSentence();
     }
@@ -45,6 +71,4 @@ public class AnimatedText : MonoBehaviour
     {
         SceneManager.LoadScene("level0");
     }
-
-    
 }
